049f033a913549b6b17150ae153ff3880379eb0e
ohoo2454 <66541536@qq.com>
2020-06-12 17:02:32 +0800

    django example1 200612

---
+++ myProject/myApp/views.py
@@ -0,0 +1,8 @@
+# myApp/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world!")
+
