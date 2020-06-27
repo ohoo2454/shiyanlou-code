a1344c3f98a408b91558718aea9f8a2f462afc5f
ohoo2454 <66541536@qq.com>
2020-06-27 17:46:44 +0800

    200627_stack

---
+++ python-files/data_structure/stack_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+
+class Stack(object):
+    def __init__(self, limit=10):
+        self.stack = []
+        self.limit = limit
+
+    def push(self, data):
+        if (len(self.stack) >= self.limit):
+            raise IndexError('Out of the range of the stack limit!')
+        self.stack.append(data)
+
+    def pop(self):
+        if (self.stack):
+            return self.stack.pop()
+        else:
+            raise IndexError('Pop from an empty stack!')
+
+    def peek(self):
+        if (self.stack):
+            return self.stack[-1]
+
+    def is_empty(self):
+        return not bool(self.stack)
+
+    def size(self):
+        return len(self.stack)
+
