8706b1140c620871619e4c2e55c3ce56d1a44326
ohoo2454 <66541536@qq.com>
2020-07-30 10:15:50 +0800

    200730_nude_finish

--- python-files/Nude/nude.py
+++ python-files/Nude/nude.py
@@ -104,4 +104,208 @@ class Nude(object):
         return self
 
     def _classify_skin(self, r, g, b):
+        rgb_classifier = r > 95 and \
+            g > 40 and g < 100 and \
+            b > 20 and \
+            max([r, g, b]) - min([r, g, b]) > 15 and \
+            abs(r - g) > 15 and \
+            r > g and \
+            r > b
+        
+        nr, ng, nb = self._to_normalized(r, g, b)
+        norm_rgb_classifier = nr / ng > 1.185 and \
+            float(r * b) / ((r + g + b) ** 2) > 0.107 and \
+            float(r * g) / ((r + g + b) ** 2) > 0.112
+        
+        h, s, v = self._to_hsv(r, g, b)
+        hsv_classifier = h > 0 and \
+            h < 35 and \
+            s > 0.23 and \
+            s < 0.68
+        
+        y, cb, cr = self._to_ycbcr(r, g, b)
+        ycbcr_classifier = 97.5 <= cb <= 142.5 and \
+            134 <= cr <= 176
+        
+        # return rgb_classifier or norm_rgb_classifier or hsv_classifier or ycbcr_classifier
+        return ycbcr_classifier
+
+    def _to_normalized(self, r, g, b):
+        if r == 0:
+            r = 0.0001
+        if g == 0:
+            g = 0.0001
+        if b == 0:
+            b = 0.0001
+        _sum = float(r + g + b)
+        
+        return [r / _sum, g / _sum, b / _sum]
+    
+    def _to_ycbcr(self, r, g, b):
+        y = .299*r + .587*g + .114*b
+        cb = 128 - 0.168736*r - 0.331364*g + 0.5*b
+        cr = 128 + 0.5*r - 0.418688*g - 0.081312*b
+        
+        return y, cb, cr
+    
+    def _to_hsv(self, r, g, b):
+        h = 0
+        _sum = float(r + g + b)
+        _max = float(max([r, g, b]))
+        _min = float(min([r, g, b]))
+        diff = float(_max - _min)
+        if _sum == 0:
+            _sum = 0.0001
+        
+        if _max == r:
+            if diff == 0:
+                h = sys.maxsize
+            else:
+                h = (g - b) / diff
+        elif _max == g:
+            h = 2 + ((g - r) / diff)
+        else:
+            h = 4 + ((r - g) / diff)
+
+        h *= 60
+        if h < 0:
+            h += 360
+        
+        return [h, 1.0 - (3.0 * (_min /_sum )), (1.0 / 3.0) * _max]
+
+    def _add_merge(self, _from, _to):
+        self.last_from = _from
+        self.last_to = _to
+
+        from_index = -1
+        to_index = -1
+
+        for index, region in enumerate(self.merge_regions):
+            for r_index in region:
+                if r_index == _from:
+                    from_index = index
+                if r_index == _to:
+                    to_index = index
+        
+        if from_index != -1 and to_index != -1:
+            if from_index != to_index:
+                self.merge_regions[from_index].extend(self.merge_regions[to_index])
+                del(self.merge_regions[to_index])
+            return
+
+        if from_index == -1 and to_index == -1:
+            self.merge_regions.append([_from, _to])
+            return
+        
+        if from_index != -1 and to_index == -1:
+            self.merge_regions[from_index].append(_to)
+            return
+        
+        if from_index == -1 and to_index != -1:
+            self.merge_regions[to_index].append(_from)
+            return
+        
+    def _merge(self, detected_regions, merge_regions):
+        new_detected_regions = []
+
+        for index, region in enumerate(merge_regions):
+            try:
+                new_detected_regions[index]
+            except IndexError:
+                new_detected_regions.append([])
+            for r_index in region:
+                new_detected_regions[index].extend(detected_regions[r_index])
+                detected_regions[r_index] = []
             
+        for region in detected_regions:
+            if len(region) > 0:
+                new_detected_regions.append(region)
+        
+        self._clear_regions(new_detected_regions)
+
+    def _clear_regions(self, detected_regions):
+        for region in detected_regions:
+            if len(region) > 30:
+                self.skin_regions.append(region)
+    
+    def _analyse_regions(self):
+        if len(self.skin_regions) < 3:
+            self.message = "Less than 3 skin regions ({_skin_regions_size})".format(
+                _skin_regions_size=len(self.skin_regions))
+            self.result = False
+            return self.result
+        
+        self.skin_regions = sorted(self.skin_regions, key=lambda s: len(s),
+                                   reverse=True)
+        total_skin = float(sum([len(skin_region) for skin_region in self.skin_regions]))
+
+        if total_skin / self.total_pixels * 100 < 15:
+            self.message = "Total skin percentage lower than 15 ({:.2f})".format(total_skin / self.total_pixels * 100)
+            self.result = False
+            return self.result
+        
+        if len(self.skin_regions[0]) / total_skin * 100 < 45:
+            self.message = "The biggest region contains less than 45 ({:.2f})".format(len(self.skin_regions[0]) / total_skin * 100)
+            self.result = False
+            return self.result
+
+        if len(self.skin_regions) > 60:
+            self.message = "More than 60 skin regions ({})".format(len(self.skin_regions))
+            self.result = False
+            return self.result
+        
+        self.message = "Nude!!"
+        self.result = True
+        return self.result
+
+    def inspect(self):
+        _image = '{} {} {}x{}'.format(self.image.filename, self.image.format, self.width, self.height)
+        return "{_image}: result={_result} message='{_message}'".format(_image=_image, _result=self.result, _message=self.message)
+
+    def showSkinRegions(self):
+        if self.result is None:
+            return
+        
+        skinIdSet = set()
+        simage = self.image
+        simageDate = simage.load()
+
+        for sr in self.skin_regions:
+            for pixel in sr:
+                skinIdSet.add(pixel.id)
+        for pixel in self.skin_map:
+            if pixel.id not in skinIdSet:
+                simageDate[pixel.x, pixel.y] = 0, 0, 0
+            else:
+                simageDate[pixel.x, pixel.y] = 255, 255, 255
+        filePath = os.path.abspath(self.image.filename)
+        fileDirectory = os.path.dirname(filePath) + '/'
+        fileFullName = os.path.basename(filePath)
+        fileName, fileExtName = os.path.splitext(fileFullName)
+        simage.save('{}{}_{}{}'.format(fileDirectory, fileName, "Node" if self.result else "Normal", fileExtName))
+    
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Detect nudity in images.')
+    parser.add_argument('files', metavar='image', nargs='+',
+                        help='Images you wish to test')
+    parser.add_argument('-r', '--resize', action='store_true',
+                        help='Reduce image size to increase speed of scanning')
+    parser.add_argument('-v', '--visualization', action='store_true',
+                        help='Generating areas of skin image')
+    
+    args = parser.parse_args()
+
+    for fname in args.files:
+        if os.path.isfile(fname):
+            n = Nude(fname)
+            if args.resize:
+                n.resize(maxheight=800, maxwidth=600)
+            n.parse()
+            if args.visualization:
+                n.showSkinRegions()
+            print(n.result, n.inspect())
+        else:
+            print(fname, "is not a file")
